96e128d508e4c65fcad7b1a181e6ac527eafd4f2
ErvinCs <csoka.ervin12@gmail.com>
2018-12-12 12:02:51 +0200

    Lab08 - does not work on all inputs

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,42 @@
-import problem1.Sequence;
-import problem1.SequenceSumOperation;
-import problem2.BigNumber;
-import problem2.BigNumberAddOperation;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        long startTime, stopTime, elapsedTime;
-
-        Sequence sequence = new Sequence(5);
-        System.out.println("Input " + sequence.toString());
-        SequenceSumOperation summed = new SequenceSumOperation(sequence);
-
-        startTime = System.currentTimeMillis();
-        summed.run();
-        stopTime = System.currentTimeMillis();
-
-        System.out.println("Output " + summed.toString());
-        elapsedTime = stopTime - startTime;
-        System.out.println("Time: " + elapsedTime + "ms\n");
-
-
-        BigNumber number1 = new BigNumber(new ArrayList<Integer>() {{
-            add(7);
-            add(5);
-            add(7);
-            add(8);
-        }});
-        BigNumber number2 = new BigNumber(new ArrayList<Integer>() {{
-            add(7);
-            add(7);
-            add(3);
-            add(9);
-        }});
-//        BigNumber number1 = new BigNumber();
-//        BigNumber number2 = new BigNumber();
-//        int n = 5;
-//        number1.generateDigits(n);
-//        number2.generateDigits(n);
-        BigNumberAddOperation bigSummed = new BigNumberAddOperation(number1, number2);
-
-        startTime = System.currentTimeMillis();
-        bigSummed.run();
-        stopTime = System.currentTimeMillis();
-
-        System.out.println(bigSummed.toString());
-        elapsedTime = stopTime - startTime;
-        System.out.println("Time: " + elapsedTime + "ms\n");
+    public static int[][] initMatrix(int size) {
+        int[][] paths = new int[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j ++) {
+                paths[i][j] = 0;
+            }
+        }
+        return paths;
     }
+
+    public static void main(String[] args) {
+        int size = 5;
+        int[][] testPaths = initMatrix(size);
+        //Cycle
+        testPaths[0][1] = 1;
+        testPaths[1][2] = 1;
+        testPaths[2][3] = 1;
+        testPaths[3][4] = 1;
+        testPaths[4][0] = 1;
+        //Other
+        testPaths[1][3] = 1;
+        testPaths[1][4] = 1;
+        testPaths[2][4] = 1;
+        testPaths[0][4] = 1;
+        Graph testGraph = new Graph(size, testPaths);
+
+        System.out.println(testGraph.toString() + '\n');
+
+        CycleSearcher searcher = new CycleSearcher(testGraph);
+
+        long startTime, stopTime, elapsedTime;
+        startTime = System.currentTimeMillis();
+
+        searcher.run();
+
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+        System.out.println("Time: " + elapsedTime + "ms");
+    }
+
 }
