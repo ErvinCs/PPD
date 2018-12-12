b4b06490e502455f25c6f8d28af71fdc05097739
ErvinCs <csoka.ervin12@gmail.com>
2018-12-12 12:00:16 +0200

    Lab04

--- src/main/MultiplicationThread.java
+++ src/main/MultiplicationThread.java
@@ -1,16 +1,19 @@
 package main;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
 
 public class MultiplicationThread extends Thread
 {
     private Thread thread;
     private int line, column;
-    Matrix matrix1, matrix2;
-    int result;
-    CountDownLatch latch; //Should this be here tho?
+    private Matrix matrix1, matrix2;
+    private int result;
+    private DisableLatch latch;
+//    private Object monitor;
+//    private final Condition lineReady;
 
-    public MultiplicationThread(Matrix matrix1, Matrix matrix2, int line, int column, CountDownLatch latch)
+    public MultiplicationThread(Matrix matrix1, Matrix matrix2, int line, int column, DisableLatch latch)//, Condition lineReady)//Object monitor)
     {
         this.thread = new Thread(this::run);
         this.line = line;
@@ -18,6 +21,8 @@ public class MultiplicationThread extends Thread
         this.matrix1 = matrix1;
         this.matrix2 = matrix2;
         this.latch = latch;
+        //this.lineReady = lineReady;
+        //this.monitor = monitor;
     }
 
     @Override
@@ -27,6 +32,16 @@ public class MultiplicationThread extends Thread
         for(int i = 0; i < matrix1.getColumns(); i++)
             this.result += this.matrix1.getData()[column][i] * this.matrix2.getData()[i][line];
         latch.countDown();
+        //synchronized (lineReady) {
+        //    if (latch.getCount() == 0)
+        //        lineReady.signal();
+        //}
+            //monitor.notify();
+    }
+
+    public long getLatchCount()
+    {
+        return this.latch.getCount();
     }
 
     public int getLineIndex() {
@@ -40,4 +55,8 @@ public class MultiplicationThread extends Thread
     public int getResult() {
         return result;
     }
+
+//    public Object getMonitor() {
+//        return this.monitor;
+//    }
 }
